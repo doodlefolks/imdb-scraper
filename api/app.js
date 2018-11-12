5460589b2900658fe6f44fda01ca6fefb7658e53
Andrew Lowe <alowe@hdredirect-lb3-890977680.us-east-1.elb.amazonaws.com>
2018-11-11 23:41:30 -0600

    enable cors

--- api/app.js
+++ api/app.js
@@ -1,8 +1,10 @@
 import Koa from "koa";
+import cors from "@koa/cors";
 import router from "./router";
 
 const app = new Koa();
 
+app.use(cors());
 app.use(router.middleware());
 
 app.listen(process.env.PORT || 3000);
