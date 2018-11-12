953a00aa48ad8582107daf902164b934d0f4b4dc
Andrew Lowe <alowe@hdredirect-lb3-890977680.us-east-1.elb.amazonaws.com>
2018-11-12 00:08:34 -0600

    add error handling

--- api/app.js
+++ api/app.js
@@ -5,6 +5,15 @@ import router from "./router";
 const app = new Koa();
 
 app.use(cors());
+// error handling
+app.use(async (ctx, next) => {
+  try {
+    await next();
+  } catch (ex) {
+    // todo: log exception
+    ctx.status = 500;
+  }
+});
 app.use(router.middleware());
 
 app.listen(process.env.PORT || 3000);
